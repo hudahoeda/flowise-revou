75e5fc18bf1bb9519c153fe48e5207c17292ca02
Ahmad M. Huda <ahmadmiftahulhuda@outlook.com>
2024-09-19 15:26:16 +0700

    fix security issue

--- pages_section/1_Flowise_Testing.py
+++ pages_section/1_Flowise_Testing.py
@@ -1,7 +1,6 @@
 import os
 import streamlit as st
 from Home import login,load_flowise_chat_screen, generate_custom_api_response
-from flowise import Flowise, PredictionData
 import uuid
 
 # Main content
@@ -9,8 +8,8 @@ if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
     login()
 else:
     # Using your custom API URL and Authorization header
-    API_URL = "https://flowise.revou.tech/api/v1/prediction/d6d947fc-7294-4560-a271-d4abd2ef7ef1"
-    headers = {"Authorization": "Bearer qSm0h-wPVhl8EgLzDRXLrNlt7SzQd3Dy5bw5we3d1fk"}  # Use your actual key here
+    API_URL = os.environ.get('FLOWISE_ENDPOINT')
+    headers = {"Authorization": os.environ.get('FLOWISE_KEY')}  # Use your actual key here
     assistant_message = "Asisten untuk testing Flowise via custom API"
     assistant_title = "Flowise Custom API Chat Interface"
     
